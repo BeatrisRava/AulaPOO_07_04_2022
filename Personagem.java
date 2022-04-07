82cf9143d290b62b079cd6ff56223fcaa1946b82
Bibi <>
2022-04-07 16:39:39 -0300

    Aula 07/04

--- Personagem.java
+++ Personagem.java
@@ -4,12 +4,30 @@ int energia;
 int fome;
 int sono;
 
-void caçar(){
-    System.out.printf(nome + "caçando...\n");
+final static int LIMITE_SUPERIOR = 10;
+
+void cacar(){
+    //this é instancia e não existe no método estatico (static)
+    if(this.energia >= 2){
+  System.out.printf(nome + "caçando...\n");
+  this.energia = this.energia - 2;
+    }
+    else{
+        System.out.printf("Sem energia para caçar...");
+    }
 }
     
 void comer(){
-     System.out.printf("%s comendo...\n", nome);
+    if(this.fome >= 1){
+        System.out.println(this.nome + "comendo...");
+        this.fome = this.fome - 1;
+        this.energia = this.energia + 1 <= 10 ? this.energia + 1 : this.energia;
+    }   
+    else{
+        System.out.printf("%s sem fome para comer...\n", nome);
+    }
+    this.fome = Math.min (this.fome + 1, 10);
+    this.sono = Math.min (this.sono + 1, 10);
 }
 
 void dormir(){
---
+++ jogo.java
@@ -0,0 +1,13 @@
+//se inscrever no java copilot
+
+public class jogo {
+ public static void main(String[] args) {
+        // cria um novo personagem 
+        Personagem p = new Personagem();
+        p.nome = "João";
+        p.cacar();
+        p.dormir();
+        p.comer();
+    }
+    
+}
